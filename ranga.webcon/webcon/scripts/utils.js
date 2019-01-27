492d30d5603db2ed3e5b350c151b56dc8e9a1ffb
glider0 <fengyuntianlc@outlook.com>
2019-01-27 14:23:13 +0800

    add webfont and the first initial UI framework of index.html

--- ranga.webcon/webcon/scripts/utils.js
+++ ranga.webcon/webcon/scripts/utils.js
@@ -1,11 +1,11 @@
 var utils = {};
 
-utils.formatBytes = (bytes, decimals) =>  {
-    if (bytes == 0) return '0 Bytes';
-    var k = 1024;
-    var dm = decimals <= 0 ? 0 : decimals || 2;
-    //var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    var sizes = ['字节', '千字节', '兆字节', '吉字节', '太字节', '拍字节', '艾字节', '泽字节', '尧字节'];
-    var i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+utils.formatBytes = (bytes, decimals) => {
+	if (bytes == 0) return '0 Bytes';
+	let k = 1024;
+	let dm = decimals <= 0 ? 0 : decimals || 2;
+	//let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	let sizes = ['字节', '千字节', '兆字节', '吉字节', '太字节', '拍字节', '艾字节', '泽字节', '尧字节'];
+	let i = Math.floor(Math.log(bytes) / Math.log(k));
+	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
