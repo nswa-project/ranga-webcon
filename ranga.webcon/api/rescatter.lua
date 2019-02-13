25c206f8f7a4d8a41abb5547c072e4529bfce3bb
glider0 <fengyuntianlc@outlook.com>
2019-02-13 17:01:32 +0800

    repo: purge useless files

--- ranga.webcon/api/rescatter.lua
+++
@@ -1,8 +0,0 @@
-ranga.luaload('ranga.ext.base/utils/uri.lua')
-query = uri.parsequery(os.getenv('QUERY_STRING'))
-
-print('Content-type: ' .. query['type'] .. ', charset=utf-8')
-print('')
-
-f = ranga.openfile('ranga.webcon/webcon/' .. query['file'])
-io.write(f:read("*a"))
