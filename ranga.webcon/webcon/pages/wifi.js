0eae35eb515f1017ec0ebc48249c2a00ca501627
glider0 <41747840+glider0@users.noreply.github.com>
2019-04-17 21:54:16 +0800

    page_wifi: add fast-config button to webcon

--- ranga.webcon/webcon/pages/wifi.js
+++ ranga.webcon/webcon/pages/wifi.js
@@ -2,7 +2,7 @@ var page_wifi = {};
 
 page_wifi.id = 0;
 
-page_wifi.getElementById = id => {
+page_wifi.$ = id => {
 	return document.getElementById('p-wifi-' + id);
 }
 
@@ -14,8 +14,8 @@ page_wifi.edit = () => {
 
 	if (page_wifi.id >= 0) {
 		webcon.lockScreen();
-		let channel = page_wifi.getElementById('channel'),
-			htmode = page_wifi.getElementById('htmode');
+		let channel = page_wifi.$('channel'),
+			htmode = page_wifi.$('htmode');
 
 		channel.innerHTML = '<option value="auto">自动</option>';
 		htmode.innerHTML = '';
@@ -23,7 +23,7 @@ page_wifi.edit = () => {
 		let id = "" + page_wifi.id;
 
 		ranga.api.query('wifi', ['info', id]).then(proto => {
-			page_wifi.getElementById('status').textContent = proto.payload;
+			page_wifi.$('status').textContent = proto.payload;
 			return ranga.api.query('wifi', ['cap', 'channel', id]);
 		}).then(proto => {
 			proto.payload.split('\n').forEach(i => {
@@ -51,17 +51,17 @@ page_wifi.edit = () => {
 			let data = ranga.parseProto(proto.payload + "\n\n");
 			console.log(data);
 
-			page_wifi.getElementById('chiper').value = data.chiper;
-			page_wifi.getElementById('pskssid').value = data["psk.ssid"];
-			page_wifi.getElementById('pskkey').value = data["psk.key"];
-			page_wifi.getElementById('country').value = data.country;
+			page_wifi.$('chiper').value = data.chiper;
+			page_wifi.$('pskssid').value = data["psk.ssid"];
+			page_wifi.$('pskkey').value = data["psk.key"];
+			page_wifi.$('country').value = data.country;
 
 			channel.value = data.channel;
 			htmode.value = data.htmode;
 
 			let tmp = false;
 			if (data.noscan === '1') tmp = true;
-			page_wifi.getElementById('noscan').checked = tmp;
+			page_wifi.$('noscan').checked = tmp;
 
 			editPage.classList.remove('hide');
 		}).catch(defErrorHandlerPage).finally(() => {
@@ -70,8 +70,35 @@ page_wifi.edit = () => {
 	}
 }
 
+page_wifi.restartWireless = () => {
+	webcon.lockScreen();
+	ranga.api.action('restart', ['wireless']).then(proto => {
+		dialog.simple('请耐心等待无线重启完毕，这应该不需要太长时间，但是 Web 控制台不会自动刷新。');
+	}).catch(defErrorHandler).finally(() => {
+		webcon.unlockScreen();
+	});
+}
+
 const page_wifi_init = () => {
-	let select = page_wifi.getElementById('dev');
+	webcon.addButton('快速设置', 'icon-wifi', b => {
+		page_wifi.$('easy').classList.remove('hide');
+	});
+
+	page_wifi.$('easy-apply').addEventListener('click', e => {
+		let ssid = page_wifi.$('easy-ssid').value;
+		let key = page_wifi.$('easy-key').value;
+		let suffix = (page_wifi.$('easy-suffix').checked ? '1' : '0');
+
+		webcon.lockScreen();
+		ranga.api.config('wifi', ['auto', ssid, key, suffix]).then(proto => {
+			page_wifi.restartWireless();
+			page_wifi.$('easy').classList.add('hide');
+		}).catch(defErrorHandler).finally(() => {
+			webcon.unlockScreen();
+		});
+	});
+
+	let select = page_wifi.$('dev');
 
 	select.addEventListener('change', e => {
 		page_wifi.id = parseInt(select.value);
@@ -90,16 +117,16 @@ const page_wifi_init = () => {
 		})
 	}).catch(defErrorHandlerPage);
 
-	page_wifi.getElementById('set-chiper').addEventListener('click', e => {
-		let chiper = page_wifi.getElementById('chiper').value;
+	page_wifi.$('set-chiper').addEventListener('click', e => {
+		let chiper = page_wifi.$('chiper').value;
 		ranga.api.config('wifi', ['set', '' + page_wifi.id, 'chiper', chiper]).then(proto => {
 			dialog.toast("无线设备 ‘" + page_wifi.id + "' 的加密套件已经被修改。但需要重启无线服务以生效。");
 		}).catch(defErrorHandler);
 	});
 
-	page_wifi.getElementById('set-psk').addEventListener('click', e => {
-		let ssid = page_wifi.getElementById('pskssid').value,
-			key = page_wifi.getElementById('pskkey').value;
+	page_wifi.$('set-psk').addEventListener('click', e => {
+		let ssid = page_wifi.$('pskssid').value,
+			key = page_wifi.$('pskkey').value;
 		ranga.api.config('wifi', ['set', '' + page_wifi.id, 'psk.ssid', ssid]).then(proto => {
 			return ranga.api.config('wifi', ['set', '' + page_wifi.id, 'psk.key', key]);
 		}).then(proto => {
@@ -108,11 +135,11 @@ const page_wifi_init = () => {
 
 	});
 
-	page_wifi.getElementById('set-radio').addEventListener('click', e => {
-		let country = page_wifi.getElementById('country').value,
-			channel = page_wifi.getElementById('channel').value,
-			htmode = page_wifi.getElementById('htmode').value;
-		let noscan = (page_wifi.getElementById('noscan').checked ? '1' : '0');
+	page_wifi.$('set-radio').addEventListener('click', e => {
+		let country = page_wifi.$('country').value,
+			channel = page_wifi.$('channel').value,
+			htmode = page_wifi.$('htmode').value;
+		let noscan = (page_wifi.$('noscan').checked ? '1' : '0');
 		ranga.api.config('wifi', ['set', '' + page_wifi.id, 'country', country]).then(proto => {
 			return ranga.api.config('wifi', ['set', '' + page_wifi.id, 'channel', channel]);
 		}).then(proto => {
@@ -124,9 +151,7 @@ const page_wifi_init = () => {
 		}).catch(defErrorHandler);
 	});
 
-	page_wifi.getElementById('restart').addEventListener('click', e => {
-		ranga.api.action('restart', ['wireless']).then(proto => {
-			dialog.simple('请耐心等待无线重启完毕，这应该不需要太长时间，Web 控制台不会自动刷新。');
-		}).catch(defErrorHandler);
+	page_wifi.$('restart').addEventListener('click', e => {
+		page_wifi.restartWireless();
 	});
 }
