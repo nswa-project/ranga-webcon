3b9c022ca819e51827f9449cf966b906c10dd920
glider0 <41747840+glider0@users.noreply.github.com>
2019-06-22 16:17:39 +0800

    i18n: add initial I18N support

---
+++ ranga.webcon/webcon/scripts/i18n.js
@@ -0,0 +1,51 @@
+var i18n = {};
+
+i18n.enabled = false;
+
+i18n.tr = string => {
+	if (!i18n.enabled)
+		return string;
+
+	try {
+		if (string in l10n)
+			return l10n[string];
+	} catch (e) {
+		return string;
+	}
+}
+_ = s => (i18n.tr(s))
+
+i18n.init = locale => {
+	const promise = new Promise((resolve, reject) => {
+		if (typeof value === 'undefined' || value === null || locale === '') {
+			locale = navigator.language;
+		}
+		if (locale !== "C") {
+			locale = locale.toLowerCase();
+			webcon.loadScript("l10n", "l10n/" + locale + ".js?v=__RELVERSION__").then(e => {
+				i18n.enabled = true;
+			}).catch(e => {
+				console.log("i18n: l10n data load failed (" + locale + "), I18N disabled.")
+			}).finally(() => {
+				resolve();
+			});
+		} else {
+			console.log("i18n: I18N disabled.");
+			resolve();
+		}
+	});
+	return promise;
+}
+
+i18n.runHooks = () => {
+	if (!i18n.enabled)
+		return;
+
+	let arr = document.getElementsByClassName('_tr');
+
+	for (let i = 0; i < arr.length; i++) {
+		console.log('i18n: tr: ' + arr[i].textContent);
+		arr[i].textContent = i18n.tr(arr[i].textContent);
+		arr[i].classList.remove('_tr');
+	}
+}
