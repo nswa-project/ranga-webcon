86a93aa7e38422c3022594583b7b0f646890d306
glider0 <41747840+glider0@users.noreply.github.com>
2019-08-24 18:07:24 +0800

    webcon: merge lastest patches from NSWA Maintainer's origin

--- ranga.webcon/webcon/scripts/i18n.js
+++ ranga.webcon/webcon/scripts/i18n.js
@@ -1,6 +1,6 @@
 var i18n = {};
 
-String.prototype.format = () => {
+String.prototype.format = function() {
 	let args = arguments;
 	return this.replace(/{(\d+)}/g, (match, number) => {
 		return typeof args[number] != 'undefined' ? args[number] : match;
